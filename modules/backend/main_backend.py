1f3621f3fb81410299d4463aae1a0edeab0b7781
Pat Hearps <pat.hearps@bluescope.com>
2022-03-27 21:38:13 +1100

    FEAT: basic .py modules

---
+++ modules/backend/main_backend.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from modules.common.log_config import get_logger
+
+logger = get_logger(__name__)
+
+def main():
+    logger.info("Hello I'm the backend")
+    HERE = Path(__file__).parent
+    logger.info(HERE)
+    for path in HERE.parent.iterdir():
+        print(path)
+        print(list(path.iterdir()))
+
+if __name__ == "__main__":
+    main()
---
+++ modules/common/log_config.py
@@ -0,0 +1,26 @@
+import logging
+import sys
+from typing import Union
+
+LOG_FORMAT = "%(levelname)-5s | %(asctime)s | %(module)s:%(lineno)-3s | %(message)s"
+DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
+
+
+def get_logger(name: str, level: Union[str, int] = logging.DEBUG) -> logging.Logger:
+    """
+    Sets up standardised logging formats and output. To be imported and used on a per-module basis
+
+    :param name: preferably the inbuilt python dunder variable __name__
+    :param level: logging module level, either string e.g. "DEBUG" or corresponding int e.g. 10
+    :return: ready to use logger object with standardised formatting and outputs
+    """
+    formatter = logging.Formatter(fmt=LOG_FORMAT, datefmt=DATE_FORMAT)
+
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+
+    logger.setLevel(level)
+    logger.addHandler(stream_handler)
+    return logger
