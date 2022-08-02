584f2c715847527237914999587d286e847fdb38
Pat <pat.hearps@bluescope.com>
2022-08-02 22:06:35 +1000

    DOCS: explain the two steps

--- modules/engine/main_eng.py
+++ modules/engine/main_eng.py
@@ -10,8 +10,11 @@ def main():
     HERE = Path(__file__).parent
     logger.info(f"Hello I'm the ENGINE container, located in {HERE}")
     logger.info(f"My working directory contains:")
+    # call function imported from separate common module
     show_location(HERE)
 
+    # performing dummy operation with func imported from elsewhere in engine module
+    # simply to confirm correct use of import statements
     val = randint(1, 20)
     logger.info(f"Doing math with value={val}")
     cube_square_root(val)
